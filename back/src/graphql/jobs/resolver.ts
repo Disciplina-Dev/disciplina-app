ccb2e05344368c16a4a3251d6f3eea2ab484f704
Quentin Hivanhoe <quentin.hivanhoe@epitech.eu>
2026-04-30 15:47:12 +0400

    feat(#62): update and unmatch job mutation

--- back/src/graphql/jobs/resolver.ts
+++ back/src/graphql/jobs/resolver.ts
@@ -1,18 +1,40 @@
 import { authGuard } from "../authGuard";
 import { Role } from "../../services/interfaces";
 import { JobService, toGql } from "../../services/JobService";
+import { Job, JobStatus } from "../../db/mongodb/interface";
 const jobService = new JobService();
 
 export const resolvers = {
     Query: {
         jobs: async (_: unknown, __: unknown, context: any) => {
-            // authGuard(context.user, [Role.RH])
+            authGuard(context.user, [Role.RH])
             return await jobService.findAll();
         },
 
         matchJob: async (_: unknown, { id }: { id: string}, context: any) => {
-            // authGuard(context.user, [Role.RH]);
+            authGuard(context.user, [Role.RH]);
             return await jobService.find(id);
+        }
+    },
+
+    Mutation: {
+        updateJob: async (_: unknown, { job }: { job: Job }, context: any) => {
+            authGuard(context.user, [Role.RH]);
+            const id: string = job._id;
+            job.status = JobStatus.MATCHED;
+            return await jobService.update(id, job)
+        },
+
+        unmatch: async (_: unknown, { id }: { id: string}, context: any) => {
+            authGuard(context.user, [Role.RH]);
+            const job: Job = jobService.find(id);
+
+            if (job) {
+                job.matched_candidate = [];
+                job.status = JobStatus.NOT_MATCHED;
+                return jobService.update(id, job);
+            }
+            return null
         }
     }
 }
