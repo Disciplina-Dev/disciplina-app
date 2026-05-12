b4dfb2465e0eed5eb31f9d6c760c39ad284c38c5
Quentin Hivanhoe <quentin.hivanhoe@epitech.eu>
2026-05-12 10:37:57 +0400

    refactor(#73): Remove useless endpoints keep update backend documentation

--- back/src/rest/google/route.ts
+++
@@ -1,7 +0,0 @@
-import express, { Router } from 'express';
-import { handleAuth, handleCallback } from './controller';
-
-export const router: Router = express.Router();
-
-router.get('/auth/google', handleAuth);
-router.get('/auth/callback', handleCallback);
