1ae06769a58bb8f2307cae205164f02fee56ed87
Quentin Hivanhoe <quentin.hivanhoe@epitech.eu>
2026-05-15 12:25:00 +0400

    refacto(#85): remove dead codes

--- back/src/external/google/index.ts
+++
@@ -1,5 +0,0 @@
-export { createOAuth2Client, getOAuth2Client, getAuthScopes } from './client';
-export { GoogleDriveService } from './drive.service';
-export { GoogleGmailService } from './gmail.service';
-export { buildRawMessage } from './mime.builder';
-export * from './types';
