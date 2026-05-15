8d47a338a6354bf2684d1c619dd876738a2558c2
Quentin Hivanhoe <quentin.hivanhoe@epitech.eu>
2026-05-15 12:31:05 +0400

    refactor(#85): gmail class use Google Oauth class handle refresh status and persit it in database

--- back/src/external/google/gmail.service.ts
+++ back/src/external/google/gmail.service.ts
@@ -1,36 +1,18 @@
 import { google } from 'googleapis';
-import { env } from '../../config/env';
-import { UserRepository } from '../../repositories/mysql/UserRepository';
 import { buildRawMessage } from './mime.builder';
-import { SendEmailOptions } from './types';
-
-const CALLBACK_URL = 'http://localhost:5173/auth/google';
+import { googleOAuth, GoogleOAuthClient } from './oauth-client';
+import { GoogleTokens, GoogleTokenRefreshHandler, SendEmailOptions } from './types';
 
 export class GoogleGmailService {
-    private userRepository: UserRepository;
-
-    constructor() {
-        this.userRepository = new UserRepository();
-    }
+    constructor(private readonly oauth: GoogleOAuthClient = googleOAuth) {}
 
     async sendEmail(
-        userId: number,
-        accessToken: string,
-        refreshToken: string,
+        creds: GoogleTokens,
         options: SendEmailOptions,
+        onTokenRefresh?: GoogleTokenRefreshHandler,
     ): Promise<void> {
-        const oauth2Client = new google.auth.OAuth2(env.GOOGLE_CLIENT_ID, env.GOOGLE_CLIENT_SECRET, CALLBACK_URL);
-        oauth2Client.setCredentials({ access_token: accessToken, refresh_token: refreshToken });
-
-        oauth2Client.on('tokens', async (tokens) => {
-            await this.userRepository.updateTokens(
-                userId,
-                tokens.access_token ?? accessToken,
-                tokens.refresh_token ?? refreshToken,
-            );
-        });
-
-        const gmail = google.gmail({ version: 'v1', auth: oauth2Client });
+        const auth = this.oauth.forCredentials(creds, onTokenRefresh);
+        const gmail = google.gmail({ version: 'v1', auth });
         await gmail.users.messages.send({
             userId: 'me',
             requestBody: { raw: buildRawMessage(options) },
