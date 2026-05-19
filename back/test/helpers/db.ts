ab315666cc9c85635a44466af99408c5680695ed
Quentin Hivanhoe <quentin.hivanhoe@epitech.eu>
2026-05-19 13:45:34 +0400

    test(#102): database helpers

---
+++ back/test/helpers/db.ts
@@ -0,0 +1,21 @@
+import pool from '../../src/db/mysql/connection';
+import { CandidateModel } from '../../src/db/mongo/schemas/candidate.schema';
+import { JobModel } from '../../src/db/mongo/schemas/job.schema';
+
+export async function truncateMysql(): Promise<void> {
+    const conn = await pool.getConnection();
+    try {
+        await conn.query('SET FOREIGN_KEY_CHECKS = 0');
+        await conn.query('TRUNCATE TABLE users');
+        await conn.query('TRUNCATE TABLE companies');
+        await conn.query('TRUNCATE TABLE sale_persons');
+        await conn.query('SET FOREIGN_KEY_CHECKS = 1');
+    } finally {
+        conn.release();
+    }
+}
+
+export async function dropMongo(): Promise<void> {
+    await CandidateModel.deleteMany({});
+    await JobModel.deleteMany({});
+}
