14285aa2b40440b10c020eb2039bef31df155d49
Quentin Hivanhoe <quentin.hivanhoe@epitech.eu>
2026-06-12 10:29:50 +0400

    fix(#195): company ports

--- back/src/db/mysql/connection.ts
+++ back/src/db/mysql/connection.ts
@@ -14,7 +14,7 @@ const pool: Pool =
           })
         : mysql.createPool({
               host: env.MYSQL_HOST,
-              port: 3306,
+              port: env.MYSQL_PORT,
               user: env.MYSQL_USER,
               password: env.MYSQL_ROOT_PASSWORD,
               database: env.MYSQL_DATABASE,
