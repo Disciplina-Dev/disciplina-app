c9555d3c6b0863a60ce50c6dabfa76fe52ee06d8
Quentin Hivanhoe <quentin.hivanhoe@epitech.eu>
2026-07-13 11:05:58 +0400

    fix(#304): CI broken dependencies
    remove chromium version 149 to use spartaz/chromium

--- back/test/setup.ts
+++ back/test/setup.ts
@@ -2,7 +2,7 @@ import { beforeAll, afterAll, beforeEach } from 'vitest';
 import http from 'http';
 import mongoose from 'mongoose';
 import { startServer } from '../src/index';
-import { CompanyAPI, CandidateAPI, OfferAPI } from '../src/graphql/server';
+import { CompanyAPI, CandidateAPI, OfferAPI, NeedsAnalysisAPI } from '../src/graphql/server';
 import { dropMongo } from './helpers/db';
 
 let server: http.Server;
@@ -15,12 +15,21 @@ beforeEach(async () => {
     await dropMongo();
 });
 
+function timeout<T>(p: Promise<T>, ms: number): Promise<T | undefined> {
+    return Promise.race([p, new Promise<undefined>((r) => setTimeout(r, ms))]);
+}
+
 afterAll(async () => {
-    await Promise.all([
-        CompanyAPI.stop(),
-        CandidateAPI.stop(),
-        OfferAPI.stop(),
-    ]);
-    await mongoose.disconnect();
-    await new Promise<void>((resolve) => server.close(() => resolve()));
-});
+    await timeout(CompanyAPI.stop(), 3000).catch(() => {});
+    await timeout(CandidateAPI.stop(), 3000).catch(() => {});
+    await timeout(OfferAPI.stop(), 3000).catch(() => {});
+    await timeout(NeedsAnalysisAPI.stop(), 3000).catch(() => {});
+    await timeout(mongoose.disconnect(), 3000).catch(() => {});
+    await new Promise<void>((resolve) => {
+        const timer = setTimeout(resolve, 3000);
+        server.close(() => {
+            clearTimeout(timer);
+            resolve();
+        });
+    });
+}, 20000);
